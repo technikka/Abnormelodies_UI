b66ca41e7dacae50c5d7fde865b46e9afa7b3683
Pythonikk <nikkibrusoe@gmail.com>
2023-02-14 14:51:07 -0800

    change FormMeasures to MUI components

--- src/components/FormMeasures.js
+++ src/components/FormMeasures.js
@@ -1,15 +1,33 @@
+import {
+  Grid,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  RadioGroup,
+  Slider
+} from "@mui/material";
+
 const FormMeasures = (props) => {
   return (
     <div>
-      <label htmlFor="selectedMeasures">Number of Measures</label>
-      <input
-        type="number"
-        name="selectedMeasures"
-        value = {props.num_measures}
-        min={props.minMeasures}
-        max={props.maxMeasures}
-        onChange={props.handleNumMeasuresChange}
-      />
+      <Grid container>
+        <Grid item>
+          <FormLabel>Measures</FormLabel>
+        </Grid>
+
+          <Grid item>
+            <Slider
+              value={props.num_measures}
+              onChange={props.handleNumMeasuresChange}
+              marks
+              min={props.minMeasures}
+              max={props.maxMeasures}
+              step={1}
+              valueLabelDisplay="auto"
+              style={{width: "300px"}}
+            />
+          </Grid>
+      </Grid>
     </div>
   );
 };
