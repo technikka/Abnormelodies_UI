68535b10bd62c2aa51049fb8329f8a29a338bdcd
Pythonikk <nikkibrusoe@gmail.com>
2023-04-09 13:23:54 -0700

    finish guide-- styling and wording

--- src/components/GuideModal.js
+++ src/components/GuideModal.js
@@ -14,12 +14,14 @@ import RestWholeIcon from "../icons/RestWholeIcon";
 import RestHalfIcon from "../icons/RestHalfIcon";
 import RestQuarterIcon from "../icons/RestQuarterIcon";
 import RestEighthIcon from "../icons/RestEighthIcon";
+import CloseIcon from '@mui/icons-material/Close';
 import { useTheme } from '@mui/material/styles';
 import { 
-  Modal, 
-  Box, 
   Typography, 
-  Grid 
+  Grid,
+  Dialog,
+  DialogContent,
+  IconButton,
 } from '@mui/material';
 
 const GuideModal = (props) => {
@@ -30,26 +32,34 @@ const GuideModal = (props) => {
   
   const theme = useTheme();
 
-  const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-
-    maxWidth: "600px",
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-    height: '80%',
-    overflow: 'scroll',
+  const keywordHeaderStyle = {
+    fontWeight: "bold", 
+    color: theme.palette.primary.main 
   };
 
-  const keywordHeaderStyle = {fontWeight: "bold", color: theme.palette.primary.main };
-  const keywordInnerStyle = {fontWeight: "bold", color: theme.palette.tertiary.main };
-  const timeSigStyle = {color: theme.palette.primary.main, margin: "5px 0"};
-  const backgroundStyle = {backgroundColor: theme.palette.background.main, borderRadius: "4px", padding: "5px", margin: "5px 0"};
-  const durationIconStyle = {display: "inline-block", verticalAlign: "sub", paddingRight: "5px" }
+  const keywordInnerStyle = {
+    fontWeight: "bold", 
+    color: theme.palette.tertiary.main 
+  };
+
+  const timeSigStyle = {
+    color: theme.palette.primary.main, 
+    margin: "5px 0"
+  };
+
+  const backgroundStyle = {
+    backgroundColor: theme.palette.background.main,
+    width: "100%", 
+    borderRadius: "4px", 
+    padding: "5px", 
+    margin: "5px 0"
+  };
+
+  const durationIconStyle = {
+    display: "inline-block", 
+    verticalAlign: "sub", 
+    paddingRight: "5px" 
+  }
 
   const errorTip = () => {
     return (
@@ -59,24 +69,29 @@ const GuideModal = (props) => {
 
   return (
     <Grid>
-      <Modal
+      <Dialog
         open={props.open}
         onClose={handleClose}
       >
-        <Box sx={style}>
-          <Typography variant="h6" mb={2}>
-            Melody Parameters
-          </Typography>
+        <DialogContent>
+          <div style={{display: "grid", gridTemplateColumns: "1fr auto", alignItems: "start"}}>
+            <Typography variant="h6" mb={2} style={{display: "inline-block"}}>
+              Melody Parameters
+            </Typography>
+            <IconButton onClick={handleClose} aria-label="close guide" style={{display: "inline-block", color: "rgba(0, 0, 0, 0.87)"}}>
+              <CloseIcon />
+            </IconButton>
+          </div>
           <Grid container direction="column" spacing={2}>
             <Grid item container spacing={1}>
               <Grid item><span style={keywordHeaderStyle}>Key</span> indicates the main group of notes that will form the melody, e.g., C Major will use notes C, D, E, F, G, A, and B.</Grid>
               <Grid item container spacing={1} ml={1}>
                 <Grid item style={backgroundStyle}><span style={keywordInnerStyle}>Scale</span> indicates the pattern, or <i>steps</i> used in grouping the notes. The selected scale affects the tonic options.</Grid>
-                <Grid item style={backgroundStyle}><span style={keywordInnerStyle}>Tonic</span> refers to the first note in the scale, creating the tonal center. </Grid>
+                <Grid item style={backgroundStyle}><span style={keywordInnerStyle}>Tonic</span> refers to the first note in the scale, creating the tonal center— sort of a home base, the stable note. </Grid>
               </Grid>
             </Grid>
             <Grid item container spacing={1}>
-              <Grid item><span style={keywordHeaderStyle}>Register</span> indicates the <i>range</i> of the notes, or pitches. A higher register means a higher pitch; C4 is an octave above C3, e.g., selecting C3 to C5 will allow C3, D3, and so on; C4, D4, and so on.</Grid>
+              <Grid item><span style={keywordHeaderStyle}>Register</span> indicates the <i>range</i> of the notes, or pitches. A higher register means a higher pitch; C4 is an octave above C3, e.g., selecting C3 to C5 will allow C3, D3, E3 and so on; C4, D4, E4 and so on.</Grid>
               <Grid item container spacing={1} ml={1}>
                 <Grid item style={backgroundStyle}>{errorTip()}Order matters. C begins each new octave: C D E F G A B; so selecting A3 to C3 would be nonsensical. (Lower values in the drop-down come after values before it.)</Grid>
               </Grid>
@@ -85,34 +100,34 @@ const GuideModal = (props) => {
               <Grid item><span style={keywordHeaderStyle}>Note Types</span></Grid>
               <Grid item container spacing={1} ml={1}>
                 <Grid item style={backgroundStyle}>
-                  <NoteEighthIcon style={durationIconStyle}/><span style={keywordInnerStyle}>Eighth</span> notes equal one eighth (1/8) the duration of a whole note. 
+                  <NoteEighthIcon style={durationIconStyle} /><span style={keywordInnerStyle}>Eighth</span> notes equal one eighth (1/8) the duration of a whole note. 
                 </Grid>
                 <Grid item style={backgroundStyle}>
-                  <NoteQuarterIcon style={durationIconStyle}/> <span style={keywordInnerStyle}>Quarter</span> notes equal one quarter (1/4) the duration of a whole note, or double that of an eighth note.
+                  <NoteQuarterIcon style={{...durationIconStyle, ...{paddingRight: "0"}}}/> <span style={keywordInnerStyle}>Quarter</span> notes equal one quarter (1/4) the duration of a whole note, or double that of an eighth note.
                 </Grid>
                 <Grid item style={backgroundStyle}>
                   <NoteHalfIcon style={durationIconStyle}/><span style={keywordInnerStyle}>Half</span> notes equal one half (1/2) the duration of a whole note, or double that of a quarter note.
                 </Grid>
                 <Grid item style={backgroundStyle}>
-                  <NoteWholeIcon style={durationIconStyle}/><span style={keywordInnerStyle}>Whole</span> notes are equal to the duration of 8 eighth notes, or 4 quarter notes, or 2 half notes.
+                  <NoteWholeIcon fontSize="small" style={{...durationIconStyle, ...{paddingRight: "8px"}}}/><span style={keywordInnerStyle}>Whole</span> notes are equal to the duration of 8 eighth notes, or 4 quarter notes, or 2 half notes.
                 </Grid>
                 <Grid item style={backgroundStyle}>
                   <NoteTripletIcon style={durationIconStyle}/><span style={keywordInnerStyle}>Triplet</span> notes are durationally equivalent to a half note here (there are other types not currently implemented in this version of abnormelodies). A triplet creates a unique pattern by fitting three notes into the space of a half note.
                 </Grid>
               </Grid>
               <Grid item>
-                <span style={keywordHeaderStyle}>Measures</span> are the individual sections, separated by a bar line. The amount of beats that make up a measure are indicated by the Time Signature.
+                <span style={keywordHeaderStyle}>Measures</span> are the individual sections of a piece, separated by a bar line, featuring a specific number of beats. The amount of beats that make up a measure are indicated by the Time Signature.
               </Grid>
             </Grid>
             <Grid item container spacing={1}>
               <Grid item><span style={keywordHeaderStyle}>Time Signature</span> indicates how many beats are in each measure with its upper numeral, and indicates which note type equals 1 beat with its lower numeral.</Grid>
               <Grid item container spacing={1} ml={1}>
                 <Grid item container alignItems="center" style={backgroundStyle}>
-                  <FourFourIcon style={timeSigStyle}/> time gets 4 quarter notes.
+                  <FourFourIcon style={timeSigStyle}/> time gets 4 quarter notes per measure.
                 </Grid>
                 <Grid item container style={backgroundStyle} rowSpacing={1}>
                   <Grid item container alignItems="center">
-                    <ThreeFourIcon style={timeSigStyle} /> gets 3 quarter notes.
+                    <ThreeFourIcon style={timeSigStyle} /> gets 3 quarter notes per measure.
                   </Grid>
                   
                   <Grid item>{errorTip()}Whole notes are not allowed; a whole note equals 4 beats and a measure is only 3 beats here. To allow a duration of 4 beats, see below how <i>ties</i> work.</Grid>
@@ -138,7 +153,7 @@ const GuideModal = (props) => {
               <Grid item><span style={keywordHeaderStyle}>Augmentations</span></Grid>
               <Grid item container spacing={1} ml={1}>
                 <Grid item style={backgroundStyle}>
-                  <DotIcon style={durationIconStyle}/> <span style={keywordInnerStyle}>Dots</span> extend the duration of the note they trail, by one half (1/2) that note's value, e.g., in 4/4 time, a dotted quarter note equals 1 &#189; beats. 1 beat (the value of the quarter note) + 1/2 beat (half the value of the quarter note). 
+                  <DotIcon fontSize="small" style={durationIconStyle}/> <span style={keywordInnerStyle}>Dots</span> extend the duration of the note they trail, by one half (1/2) that note's value, e.g., in 4/4 time, a dotted quarter note equals 1 &#189; beats. 1 beat (the value of the quarter note) + 1/2 beat (half the value of the quarter note). 
                 </Grid>
                 <Grid item style={backgroundStyle}>
                   <TieIcon style={durationIconStyle}/><span style={keywordInnerStyle}>Ties</span> connect two notes of the same pitch to create one duration. Two quarter notes tied together play as a half note. Ties are useful in situations where such a duration isn't normally possible, i.e., allowing a duration to cross a barline.
@@ -166,16 +181,16 @@ const GuideModal = (props) => {
               <Grid item><span style={keywordHeaderStyle}>Additional Features</span> apply rules and patterns that influence the process of melody generation. *Many more to come!</Grid>
               <Grid item container spacing={1} ml={1}>
                 <Grid item style={backgroundStyle} >
-                  <span style={keywordInnerStyle}>Smooth Resolve</span> influences the generator to prefer longer durations near the end of a melody.
+                  <span style={keywordInnerStyle}>Smooth Resolve</span> influences the generator to prefer longer durations near the end of a melody which creates the smooth resolution our ears have come to expect of a melody.
                 </Grid>
               </Grid>
             </Grid>
           </Grid>
-          <Typography variant="body2">
-            Additional tips will be indicated with a <TooltipIcon /> icon.
+          <Typography variant="body2" style={{display: "flex", alignItems: "center", margin: "15px 0"}}>
+            Additional tips will be indicated with a <TooltipIcon style={{margin: "0 5px"}}/> icon.
           </Typography>
-        </Box>
-      </Modal>
+        </DialogContent>
+      </Dialog>
     </Grid>
   )
 }
