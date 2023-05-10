f00062e8603db1ba2e187a5c919c3bd86d18f824
Pythonikk <nikkibrusoe@gmail.com>
2023-05-10 11:41:17 -0700

    lower z index to fix being on top of backdrop and feedback dialog

--- src/components/MelodyAudio.js
+++ src/components/MelodyAudio.js
@@ -183,7 +183,7 @@ const MelodyAudio = (props) => {
     bottom: "0",
     width: "100vw",
     maxWidth: theme.appMaxWidth,
-    zIndex: "10",
+    zIndex: "1",
     alignItems: "center",
     justifyItems: "center",
     boxShadow: "rgba(0, 0, 0, 0.1) 0px -2.6px 3px",
