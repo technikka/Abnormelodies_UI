a5e259c4219d2972e2320793a730abdec14ada51
Pythonikk <nikkibrusoe@gmail.com>
2023-05-13 10:09:29 -0700

    add Sec-Fetch-Site header

--- src/App.js
+++ src/App.js
@@ -123,6 +123,7 @@ const App = () => {
       .get("https://api.abnormelodies.com/api/v1/melodies", {
         params: params,
         headers: {
+          "Sec-Fetch-Site": "same-origin",
           // "Access-Control-Allow-Origin": "https://abnormelodies.com",
           Accept: "application/xml",
         },
